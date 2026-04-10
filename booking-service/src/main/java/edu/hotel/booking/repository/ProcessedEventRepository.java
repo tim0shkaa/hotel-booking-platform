1b9f6b21ebffa71f7ac895bf0670c0c2b69529fa
Timokhin Alex <a.timokhin@edu.centraluniversity.ru>
2026-04-10 20:01:46 +0300

    feat(booking): add kafka in application.yaml and add ProcessedEvent (entity, migration, repo)

---
+++ booking-service/src/main/java/edu/hotel/booking/repository/ProcessedEventRepository.java
@@ -0,0 +1,8 @@
+package edu.hotel.booking.repository;
+
+import edu.hotel.booking.entity.ProcessedEvent;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProcessedEventRepository extends JpaRepository<ProcessedEvent, Long> {
+    boolean existsByEventId(String eventId);
+}
